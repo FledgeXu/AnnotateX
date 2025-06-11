9dfb363dfa105dbfa03b70be3017236c6de5b363
Fledge Shiu <xzk0701@gmail.com>
2025-06-11 21:38:59 +0800

    Add organization exists.

--- backend/repository/organization_repository.go
+++ backend/repository/organization_repository.go
@@ -11,3 +11,13 @@ type OrganizationRepository struct {
 func NewOrganizationRepository(db *sqlx.DB) *OrganizationRepository {
 	return &OrganizationRepository{DB: db}
 }
+
+func (r *OrganizationRepository) OrganizationExists(name string) (bool, error) {
+	var exists bool
+	err := r.DB.Get(&exists, `
+		SELECT EXISTS (
+			SELECT 1 FROM users WHERE name = $1
+		)
+	`, name)
+	return exists, err
+}
