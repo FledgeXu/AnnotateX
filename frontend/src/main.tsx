9c35da0ba52b6aa9a604a3f3803fed2ce66d87fb
Fledge Shiu <xzk0701@gmail.com>
2025-06-15 16:01:47 +0800

    Import TannerQuery.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { Theme } from "@radix-ui/themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./main.css";
 import "@radix-ui/themes/styles.css";
 
@@ -18,15 +20,20 @@ declare module "@tanstack/react-router" {
     }
 }
 
+const queryClient = new QueryClient();
+
 // Render the app
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
     const root = ReactDOM.createRoot(rootElement);
     root.render(
         <StrictMode>
-            <Theme>
-                <RouterProvider router={router} />
-            </Theme>
+            <QueryClientProvider client={queryClient}>
+                <Theme>
+                    <RouterProvider router={router} />
+                </Theme>
+                {import.meta.env.DEV && <ReactQueryDevtools />}
+            </QueryClientProvider>
         </StrictMode>,
     );
 }
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -15,7 +15,6 @@ export const Route = createRootRoute({
             {/* <hr /> */}
             <Outlet />
             {import.meta.env.DEV && <TanStackRouterDevtools />}
-            <TanStackRouterDevtools />
         </>
     ),
 });
