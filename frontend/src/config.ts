19ab3ba6c93c0ed53f4163609c7ea1d078a46b64
Fledge Shiu <xzk0701@gmail.com>
2025-06-16 21:50:15 +0800

    Use store.

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,20 @@
 import axios from "axios";
-export const API = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL,
-    timeout: 360,
-});
+import type { Store } from "easy-peasy";
+import type { StoreModel } from "./store/types";
+
+export const createAPI = (store: Store<StoreModel>) => {
+    const api = axios.create({
+        baseURL: import.meta.env.VITE_BASE_URL,
+        timeout: 360,
+    });
+
+    api.interceptors.request.use((config) => {
+        const token = store.getState().auth.token;
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    })
+
+    return api;
+}
