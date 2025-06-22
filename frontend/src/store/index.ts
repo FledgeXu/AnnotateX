0cc55fc85bc1db3e34419b9a135404efbdea228d
Fledge Shiu <xzk0701@gmail.com>
2025-06-22 20:01:22 +0800

    Persist token.

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,7 +1,15 @@
-import { createStore } from "easy-peasy";
+import { createStore, persist } from "easy-peasy";
 import { authModel } from "@/store/models/authModel";
 import type { StoreModel } from "@/store/types";
 
-export const store = createStore<StoreModel>({
-    auth: authModel,
-});
+export const store = createStore<StoreModel>(
+    persist(
+        {
+            auth: authModel,
+        },
+        {
+            allow: ["auth"],
+            storage: localStorage,
+        },
+    )
+);
