683c291ebda683f593cf54961015c43cb492b7ee
Fledge Shiu <xzk0701@gmail.com>
2025-06-25 17:19:40 +0800

    Add response.go

---
+++ backend/utils/response.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+func JSONResponse(c *gin.Context, httpCode int, message string, data interface{}) {
+	if data == nil {
+		data = gin.H{}
+	}
+	c.JSON(httpCode, Response{
+		Code:    httpCode,
+		Message: message,
+		Data:    data,
+	})
+}
+
+func JSONSuccess(c *gin.Context, httpCode int, data interface{}) {
+	JSONResponse(c, httpCode, "success", data)
+}
+
+func JSONError(c *gin.Context, httpCode int, code int, message string) {
+	JSONResponse(c, code, message, nil)
+}
