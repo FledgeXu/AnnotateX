b9173b26cdd3f818963961591309c1097c8b67da
Fledge Shiu <xzk0701@gmail.com>
2025-06-30 21:34:42 +0800

    Separate ProjectsList.

--- frontend/src/components/pages/ProjectSidebar.tsx
+++ frontend/src/components/pages/ProjectSidebar.tsx
@@ -6,13 +6,37 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { createAPI } from "@/config";
-import type { ProjectResponse, Response } from "@/models";
+import type { Project, ProjectResponse, Response } from "@/models";
 import { store } from "@/store";
 import { localizedDateFromISO } from "@/utils/date";
 const statusColorMap = new Map<string, string>([
     ["active", "bg-green-600 text-white"],
     ["archive", "bg-gray-200 text-gray-800"],
 ]);
+
+type ProjectListProps = {
+    projects: Project[];
+};
+const ProjectList = ({ projects }: ProjectListProps) => (
+    <ScrollArea className="h-full">
+        {projects.map((project, index) => (
+            <a href="/project/" key={project.id}>
+                <div className="p-2 w-full hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 rounded-sm">
+                    <div className="flex justify-between items-center">
+                        <div className="pb-2 font-medium">{project.name}</div>
+                        <Badge className={statusColorMap.get(project.status)}>
+                            {project.status}
+                        </Badge>
+                    </div>
+                    <span className="text-sm text-gray-500">
+                        {localizedDateFromISO(project.created_at)}
+                    </span>
+                </div>
+                {index < projects.length - 1 && <Separator className="m-2" />}
+            </a>
+        ))}
+    </ScrollArea>
+);
 
 export const ProjectSidebar = () => {
     const { isPending, error, data, isFetching } = useQuery<
@@ -40,26 +64,7 @@ export const ProjectSidebar = () => {
                 startIcon={<SearchIcon className="w-4 h-4" />}
             />
             <div className="flex-1 min-h-0">
-                <ScrollArea className="flex-1 h-full">
-                    {data?.data.results.map((project, index) => (
-                        <a href="/project/" key={project.id}>
-                            <div className="p-2 w-full hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 rounded-sm">
-                                <div className="flex justify-between items-center">
-                                    <div className="pb-2 font-medium">{project.name}</div>
-                                    <Badge className={statusColorMap.get(project.status)}>
-                                        {project.status}
-                                    </Badge>
-                                </div>
-                                <span className="text-sm text-gray-500">
-                                    {localizedDateFromISO(project.created_at)}
-                                </span>
-                            </div>
-                            {index < data?.data.results.length - 1 && (
-                                <Separator className="m-2" />
-                            )}
-                        </a>
-                    ))}
-                </ScrollArea>
+                {data && <ProjectList projects={data.data.results} />}
             </div>
         </div>
     );
