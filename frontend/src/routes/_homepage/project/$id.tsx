8258c3d1fc3d5c56adc3a7c299d2359e108e97ab
Fledge Shiu <xzk0701@gmail.com>
2025-07-05 11:47:43 +0800

    Add fetching project.

--- frontend/src/routes/_homepage/project/$id.tsx
+++ frontend/src/routes/_homepage/project/$id.tsx
@@ -1,7 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
 import { createFileRoute, useParams } from "@tanstack/react-router";
 import { DatasetPage } from "@/components/pages/DatasetPage";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { createAPI } from "@/config";
+import type { Project } from "@/models";
+import { store } from "@/store";
 
 export const Route = createFileRoute("/_homepage/project/$id")({
   component: RouteComponent,
@@ -9,10 +13,19 @@ export const Route = createFileRoute("/_homepage/project/$id")({
 
 function RouteComponent() {
   const { id } = useParams({ strict: false });
+  const { data: project } = useQuery<Project>({
+    queryKey: ["fetchProjectInfo", id],
+    queryFn: async () => {
+      const api = createAPI(store);
+      const res = await api.get(`/v1/projects/${id}`);
+      return res.data.data;
+    },
+  });
+
   return (
     <Tabs defaultValue="account">
       <div className="flex justify-between items-center">
-        <span className="text-2xl font-semibold">{id}</span>
+        <span className="text-2xl font-semibold">{project?.name ?? ""}</span>
         <TabsList>
           <TabsTrigger value="batches">Batches</TabsTrigger>
           <TabsTrigger value="dataset">Dataset</TabsTrigger>
