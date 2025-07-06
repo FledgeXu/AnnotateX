7b4a7da24e559a5bd1bfd5671945287346f6acca
Fledge Shiu <xzk0701@gmail.com>
2025-07-06 17:43:05 +0800

    refactor backend.

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,4 @@
 package main
 
-import (
-	"annotate-x/config"
-	"annotate-x/internal/auth"
-
-	"annotate-x/router"
-)
-
-func initialize() {
-	auth.InitCasbinEnforcer()
-}
-
 func main() {
-	initialize()
-	r := router.SetupRouter()
-	r.Run(config.AppConfig.LISTEN_ADDRESS)
 }
