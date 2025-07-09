22de3bc8f82f6114180b22707937e9656b31cc76
Fledge Shiu <xzk0701@gmail.com>
2025-07-10 00:31:26 +0800

    Add graceful.

--- backend/go.mod
+++ backend/go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/cloudwego/base64x v0.1.5 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
+	github.com/gin-contrib/graceful v1.1.4 // indirect
 	github.com/gin-contrib/gzip v1.2.3 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
--- backend/main.go
+++ backend/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"annotate-x/config"
 	"annotate-x/router"
+
+	"github.com/gin-contrib/graceful"
 )
 
 func main() {
-	r := router.SetupRouter()
-	r.Run(config.AppConfig.LISTEN_ADDRESS)
+	router := router.SetupRouter()
+
+	g, err := graceful.New(router)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := g.Run(config.AppConfig.LISTEN_ADDRESS); err != nil {
+		panic(err)
+	}
 }
