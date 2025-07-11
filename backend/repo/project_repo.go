f8241fc6afc75468bf87b7dfd76fc26e78dd2be8
Fledge Shiu <xzk0701@gmail.com>
2025-07-12 00:41:13 +0800

    Add create project.

--- backend/repo/project_repo.go
+++ backend/repo/project_repo.go
@@ -3,12 +3,13 @@ package repo
 import (
 	"annotate-x/models"
 	"context"
+	"database/sql"
 
 	"github.com/jmoiron/sqlx"
 )
 
 type IProjectRepo interface {
-	CreateProject(ctx context.Context, projectReq *models.CreateProjectRequest) (*models.Project, error)
+	CreateProject(ctx context.Context, projectReq *models.CreateProjectRequest) error
 	GetProjectByID(ctx context.Context, id int64) (*models.Project, error)
 	ListProjects(ctx context.Context, filter models.ProjectFilter) ([]*models.Project, error)
 	UpdateProject(ctx context.Context, project *models.Project) error
@@ -24,13 +25,22 @@ func NewProjectRepo(db *sqlx.DB) *ProjectRepo {
 	return &ProjectRepo{DB: db}
 }
 
-func (r *ProjectRepo) CreateProject(ctx context.Context, projectReq *models.CreateProjectRequest) (*models.Project, error) {
-	var project *models.Project
+func (r *ProjectRepo) CreateProject(ctx context.Context, projectReq *models.CreateProjectRequest) error {
 	query := `
 		INSERT INTO projects (name, modality, description)
 		VALUES (:name, :modality, :description) 
 		RETURNING id, name, modality, status, description, created_at, updated_at
 	`
-	err := r.DB.GetContext(ctx, &project, query, projectReq)
-	return project, err
+	result, err := r.DB.NamedExecContext(ctx, query, projectReq)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+	return nil
 }
--- backend/repo/project_repo_test.go
+++ backend/repo/project_repo_test.go
@@ -1,40 +1,30 @@
 package repo_test
 
-//
-// import (
-// 	"annotate-x/models"
-// 	"annotate-x/repo"
-// 	"context"
-// 	"testing"
-// 	"time"
-//
-// 	"github.com/DATA-DOG/go-sqlmock"
-// 	"github.com/stretchr/testify/assert"
-// )
-//
-// func TestCreateProject(t *testing.T) {
-// 	db, mock := setupMockDB(t)
-// 	repo := repo.NewProjectRepo(db)
-// 	context := context.Background()
-//
-// 	projectReq := &models.CreateProjectRequest{
-// 		Name:        "testproject",
-// 		Modality:    "2D",
-// 		Description: "test description",
-// 	}
-//
-// 	mock.ExpectPrepare("INSERT INTO projects").
-// 		ExpectQuery().
-// 		WithArgs(projectReq.Name, projectReq.Modality, projectReq.Description).
-// 		WillReturnRows(
-// 			sqlmock.NewRows([]string{
-// 				"id", "name", "modality", "status", "description", "created_at", "updated_at",
-// 			}).AddRow(
-// 				1, "Demo", "image", "active", "Test project", time.Now(), time.Now(),
-// 			),
-// 		)
-//
-// 	project, err := repo.CreateProject(context, projectReq)
-// 	assert.NoError(t, err)
-// 	assert.Equal(t, int64(1), project.ID)
-// }
+import (
+	"annotate-x/models"
+	"annotate-x/repo"
+	"context"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateProject(t *testing.T) {
+	db, mock := setupMockDB(t)
+	repo := repo.NewProjectRepo(db)
+	context := context.Background()
+
+	projectReq := &models.CreateProjectRequest{
+		Name:        "testproject",
+		Modality:    "2D",
+		Description: "test description",
+	}
+
+	mock.ExpectExec("INSERT INTO projects").
+		WithArgs(projectReq.Name, projectReq.Modality, projectReq.Description).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	err := repo.CreateProject(context, projectReq)
+	assert.NoError(t, err)
+}
