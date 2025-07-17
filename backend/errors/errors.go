b8a7135727bd0c61b26b8676098dbdec93733560
Fledge Shiu <xzk0701@gmail.com>
2025-07-17 23:48:56 +0800

    Add BadRequestError.

---
+++ backend/errors/errors.go
@@ -0,0 +1,13 @@
+package errors
+
+type BadRequestError struct {
+	message string
+}
+
+func (e *BadRequestError) Error() string {
+	return e.message
+}
+
+func NewBadRequestError(msg string) error {
+	return &BadRequestError{message: msg}
+}
