2bf0026605bfcbdcf1baf8dac4beab056c19fdd9
Fledge Shiu <xzk0701@gmail.com>
2025-07-19 22:30:34 +0800

    Auto close db.

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"annotate-x/config"
+	"annotate-x/db"
 	"annotate-x/router"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gin-contrib/graceful"
 )
+
+func init() {
+	db := db.InitDB(config.GetConfig().DATABASE_URL)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-quit
+		log.Println("Gracefully shutting down...")
+
+		db.Close()
+
+		os.Exit(0)
+	}()
+}
 
 func main() {
 	router := router.SetupRouter()
